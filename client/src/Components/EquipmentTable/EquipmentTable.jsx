37e001f9a58ecf1e2734b6c022408aa26d64985b
Matei Teca <matei.teca@gmail.com>
2023-03-20 17:58:19 +0200

    store input values

--- client/src/Components/EquipmentTable/EquipmentTable.jsx
+++ client/src/Components/EquipmentTable/EquipmentTable.jsx
@@ -1,23 +1,42 @@
 import { Link } from "react-router-dom";
+import React, {useState} from "react"
 
-const EquipmentTable = () => (
+const EquipmentTable = () => {
 
-  <div className="EmployeeTable">
+  const [name, setName] = useState("");
+  const [type, setType] = useState("");
+  const [amount, setAmount] = useState("");
+
+  const handleNameInput = (e) => {
+    setName(e.target.value);
+  }
+
+  const handleTypeInput = (e) => {
+    setType(e.target.value);
+  }
+
+  const handleAmountInput = (e) => {
+    setAmount(e.target.value);
+  }
+
+
+  return (
+    <div className="EmployeeTable">
     <table>
       <thead>
         <tr>
           <th>Name</th>
-          <th>Level</th>
-          <th>Position</th>
+          <th>Type</th>
+          <th>Amount</th>
           <th />
         </tr>
       </thead>
       <tbody>
 
           <tr>
-            <td><input/></td>
-            <td><input/></td>
-            <td><input/></td>
+            <td><input type = "text" placeholder = "Name" onChange={handleNameInput}/></td>
+            <td><input type = "text" placeholder = "Type" onChange={handleTypeInput}/></td>
+            <td><input type = "text" placeholder = "Amount" onChange={handleAmountInput}/></td>
             <td>
               <button type="button" onClick={() => alert("nothing yet")}>
                 Add equipment
@@ -39,6 +58,8 @@ const EquipmentTable = () => (
       </tbody>
     </table>
   </div>
-);
+  )
+}
+
 
 export default EquipmentTable;

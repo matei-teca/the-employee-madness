70e3a2887ad1a39dcba205dae49fcb95c358863a
Matei Teca <matei.teca@gmail.com>
2023-03-20 14:08:27 +0200

    Extend the list with filter features

--- client/src/Components/SearchBy/SearchBy.jsx
+++ client/src/Components/SearchBy/SearchBy.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, {useRef, useEffect} from 'react';
+import { useAtom } from "jotai";
+import state from "../../AtomStates";
 
 export default function SearchBy() {
+
+    const [employees, setEmployees] = useAtom(state.employees);
+    const inputRef = useRef(null);
+
+    const handleByPosition = () => {
+
+        fetch("/api/employees").then((res) => res.json())
+            .then((employees) => {
+                setEmployees(employees);
+                setEmployees((employees) => {
+                    return employees.filter((employee) => employee.position.toUpperCase() === inputRef.current.value.toUpperCase());
+                })
+        })
+    }
+
+    const handleByLevel = () => {
+
+        fetch("/api/employees").then((res) => res.json())
+            .then((employees) => {
+                setEmployees(employees);
+                setEmployees((employees) => {
+                    return employees.filter((employee) => employee.level.toUpperCase() === inputRef.current.value.toUpperCase());
+                })
+        })
+    }
+
   return (
     <div>
         {/* <div>SearchBy</div> */}
-        <input type="text" placeholder="choose a Position or Level" className='input'/>
-        <button>Filter by Position</button>
-        <button>Filter by Level</button>
+        <input ref = {inputRef} type="text" placeholder="choose a Position or Level" className='input'/>
+        <button onClick={handleByPosition}>Filter by Position</button>
+        <button onClick={handleByLevel}>Filter by Level</button>
     </div>
   )
 }

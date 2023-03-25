242b460a3560ee4f25c2156331b4d01613afa3ab
Matei Teca <matei.teca@gmail.com>
2023-03-25 13:40:06 +0200

    integrated navbar

--- client/src/Components/EmployeesListNavBar/EmployeesListNavBar.jsx
+++ client/src/Components/EmployeesListNavBar/EmployeesListNavBar.jsx
@@ -8,103 +8,110 @@ import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Button from "react-bootstrap/Button";
 
-const EmployeesListNavBar = ({headerName}) => (
+const EmployeesListNavBar = ({ headerName }) => (
   <div className="Layout">
     <nav>
       <ul>
-        <li className="grow">
-          <Link to="/">{headerName}</Link>
-        </li>
-        <li>
-          <Link to="/create">
-            <button type="button">Create Employee</button>
-          </Link>
-        </li>
-      </ul>
-    </nav>
-    <Navbar
-        className="navbar"
-        variant="dark"
-        expand="lg"
-        style={{ display: "flex", justifyContent: "center", width: "100%" }}
-      >
-        <Container fluid>
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
-            navbarScroll
-          >
-            <Nav.Link>Diary</Nav.Link>
-          </Nav>
-          <Form className="d-flex">
-            <div className="search-bar--container">
-              <div className="test">
-                <Form.Control
-                  // ref={inputValue}
-                  // list="search-bar--datalist"
-                  id="search-bar--input"
-                  type="search"
-                  placeholder="products search"
-                  style={{ width: "13vw", marginLeft: "5vw" }}
-                  onKeyDown={(e) => {
-                    if (e.key === "Enter") {
-                      e.preventDefault();
-                      // searchByName(
-                      //   inputValue.current.value,
-                      //   setSearchNames,
-                      //   setProduct
-                      // );
-                    }
-                  }}
-                />
-                <div style={{ display: "flex", flexDirection: "column" }}></div>
-                <NavDropdown
-                  title="Search by"
-                  id="basic-nav-dropdown"
-                  style={{ width: "17vw", marginLeft: "1vw" }}
-                >
-                  <NavDropdown.Item
-                    style={{ backgroundColor: "rgba(0,0,0,0)" }}
-                  >
-                    <Button
-                      onClick={() =>
+        <Navbar
+          variant="dark"
+          expand="lg"
+          style={{ display: "flex", justifyContent: "center", width: "100%" }}
+        >
+          <Container fluid style={{ display: "flex" }}>
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <li className="grow">
+                <Link to="/">{headerName}</Link>
+              </li>
+            </Nav>
+            <Form className="d-flex">
+              <div
+              >
+                <div style={{width: "700px", display:"flex"}}>
+                  <Form.Control
+                    // ref={inputValue}
+                    // list="search-bar--datalist"
+                    id="search-bar--input"
+                    type="search"
+                    placeholder="products search"
+                    style={{ width: "13vw", marginLeft: "5vw" }}
+                    onKeyDown={(e) => {
+                      if (e.key === "Enter") {
+                        e.preventDefault();
                         // searchByName(
                         //   inputValue.current.value,
                         //   setSearchNames,
                         //   setProduct
-                        // )
-                        console.log("button1")
+                        // );
                       }
-                      variant="secondary"
-                      style={{ width: "100%", fontSize: "0.8rem" }}
-                    >
-                      by name
-                    </Button>
-                  </NavDropdown.Item>
-                  <NavDropdown.Item
-                    style={{ backgroundColor: "rgba(0,0,0,0)" }}
+                    }}
+                  />
+                  <div
+                    style={{ display: "flex", flexDirection: "column" }}
+                  ></div>
+                  <NavDropdown
+                    title="Search by"
+                    id="basic-nav-dropdown"
+                    style={{ width: "17vw", marginLeft: "1vw" }}
                   >
-                    <Button
-                      onClick={() =>
-                        // searchByBarcode(
-                        //   inputValue.current.value,
-                        //   setSearchNames,
-                        //   setProduct
-                        // )
-                        console.log("button2")
-                      }
-                      variant="secondary"
-                      style={{ width: "100%", fontSize: "0.8rem" }}
+                    <NavDropdown.Item
+                      style={{ backgroundColor: "rgba(0,0,0,0)" }}
                     >
-                      by barcode
-                    </Button>
-                  </NavDropdown.Item>
-                </NavDropdown>
+                      <Button
+                        onClick={() =>
+                          // searchByName(
+                          //   inputValue.current.value,
+                          //   setSearchNames,
+                          //   setProduct
+                          // )
+                          console.log("button1")
+                        }
+                        variant="secondary"
+                        style={{ width: "100%", fontSize: "0.8rem" }}
+                      >
+                        by name
+                      </Button>
+                    </NavDropdown.Item>
+                    <NavDropdown.Item
+                      style={{ backgroundColor: "rgba(0,0,0,0)" }}
+                    >
+                      <Button
+                        onClick={() =>
+                          // searchByBarcode(
+                          //   inputValue.current.value,
+                          //   setSearchNames,
+                          //   setProduct
+                          // )
+                          console.log("button2")
+                        }
+                        variant="secondary"
+                        style={{ width: "100%", fontSize: "0.8rem" }}
+                      >
+                        by barcode
+                      </Button>
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </div>
               </div>
-            </div>
-          </Form>
-        </Container>
-      </Navbar>
+            </Form>
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <li>
+                <Link to="/create">
+                  <button type="button">Create Employee</button>
+                </Link>
+              </li>
+            </Nav>
+          </Container>
+        </Navbar>
+      </ul>
+    </nav>
     <Outlet />
   </div>
 );

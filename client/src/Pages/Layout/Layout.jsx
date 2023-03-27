f263254d656e2c38c57b111f5561287b4f5f62b3
Matei Teca <matei.teca@gmail.com>
2023-03-27 08:03:49 +0300

    added icon

--- client/src/Pages/Layout/Layout.jsx
+++ client/src/Pages/Layout/Layout.jsx
@@ -1,12 +1,28 @@
 import { Outlet, Link } from "react-router-dom";
+import {useState} from "react";
 import "./Layout.css";
 
-const Layout = () => (
+const Layout = () => {
+
+const [bool, setBool] = useState(false);
+const handleImgDbClick = (e) => {
+  setBool(prev => !prev)
+  e.target.style.width = bool ? "200px" : "25px"
+  e.target.style.height = bool ? "200px" : "25px"
+  e.target.style.marginTop = bool ? "90px" : "0px"
+}
+
+return(
   <div className="Layout" style ={{height:"auto", minHeight:"100vh", position:"relative"}}>
     <nav>
         <ul style ={{height:"40px", backgroundColor:"navy"}}>
           <li className="grow">
             <Link to="/">The Employee Madness</Link>
+            <img src="https://media.tenor.com/images/a1d1ba09fbd871e0bc44990bab058d8d/tenor.png" 
+            width={25} height={25} 
+            style ={{marginLeft: "10px", borderRadius: "450%"}}
+            onDoubleClick = {(e) => handleImgDbClick(e)}
+            />
           </li>
           {/* <li>
             <Link to="/create">
@@ -21,5 +37,6 @@ const Layout = () => (
     </div>
   </div>
 );
+        }
 
 export default Layout;
